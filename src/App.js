cde6319d4fbbab1a1723aca61893b399ffee7f14
2624789 <2624789@users.noreply.github.com>
2023-03-18 10:53:16 -0500

    feature Relays component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import { useNostr } from "./nostr-context";
 
+import { Relays } from "./components/Relays";
+
 import './App.css';
-
-const Relays = () => {
-  const { connectToRelay, state } = useNostr();
-  const { relays, connectedRelay } = state;
-
-  const urls = Object.keys(relays);
-
-  if(!urls.length) return <p>None</p>;
-
-  return (urls.map(url =>
-    <p key={url}>
-      {url} - read: {JSON.stringify(relays[url].read)},
-      write: {JSON.stringify(relays[url].write)}{' '}
-      {url !== connectedRelay?.url
-        ? <button type="button" onClick={() => connectToRelay(url)}>
-            connect
-          </button>
-        : <strong> connected</strong>
-      }
-    </p>
-  ));
-}
 
 function App() {
   const { loadNostr, state } = useNostr();
@@ -38,8 +18,7 @@ function App() {
       <h1>Bounties</h1>
       <strong>Your public key is:</strong>
       <p>{publicKey ? publicKey : "None"}</p>
-      <strong>Your relays:</strong>
-      <Relays/>
+      <Relays />
     </div>
   );
 }
