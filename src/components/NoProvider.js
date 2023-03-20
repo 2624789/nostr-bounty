2d6f5e8df635aae837b6a5875d8cd2aa094f48be
2624789 <2624789@users.noreply.github.com>
2023-03-19 22:38:15 -0500

    Style NoProvider component

--- src/components/NoProvider.js
+++ src/components/NoProvider.js
@@ -4,17 +4,25 @@ import { Button } from "./Button";
 
 const FIREFOX_EXT_LINK = 'https://diegogurpegui.com/nos2x-fox/';
 const CHROME_EXT_LINK = 'https://github.com/fiatjaf/nos2x';
+const NIP07_LINK = 'https://github.com/nostr-protocol/nips/blob/master/07.md';
 
 const NoProvider = () => {
   const { loadNostr } = useNostr();
 
   return (
-    <div>
-      <Button onClick={loadNostr} label={"Load Nostr Signer Extension"} />
+    <div style={{margin: '3rem'}}>
+      <p>
+        Click <strong>Load Signer</strong> and allow Bounties Manager to read
+        {' '}your public key and relays to start.
+      </p>
+      <Button onClick={loadNostr} label={"Load Signer"} />
       <p>
         <small>
-          Install for <a href={CHROME_EXT_LINK}>Chrome</a>
-          {' '}or <a href={FIREFOX_EXT_LINK}>Firefox</a>
+          Bounties Manager uses <a href={NIP07_LINK}>NIP-07</a> signer
+          {' '}extensions to let you sign Nostr events without giving it access
+          {' '}to your private keys. Install extension for
+          {' '}<a href={CHROME_EXT_LINK}>Chrome</a> or
+          {' '}<a href={FIREFOX_EXT_LINK}>Firefox</a>.
         </small>
       </p>
     </div>
