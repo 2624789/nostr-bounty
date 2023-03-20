1e1f8b7b32d14d68d30bb3ca05b9a9845cb5c83c
2624789 <2624789@users.noreply.github.com>
2023-03-20 00:43:22 -0500

    Use fieldset in PublicKey

--- src/components/PublicKey.js
+++ src/components/PublicKey.js
@@ -3,16 +3,14 @@ import { useNostrState } from "./../nostr-context";
 function PublicKey() {
   const { publicKey } = useNostrState();
 
-  return (
-    <p>
-      {publicKey
-        ? <span>
-            Your public key is:
-            {' '}<strong style={{color: '#ED9B33'}}>{publicKey}</strong>
-          </span>
-        : <strong style={{color: '#E0457B'}}>Unknown public key</strong>
-      }
-    </p>
+  return (publicKey
+    ? <fieldset>
+        <legend style={{color: '#5461C8'}}>
+          <strong>Your public key is:</strong>
+        </legend>
+        <strong style={{color: '#ED9B33'}}>{publicKey}</strong>
+      </fieldset>
+    : <strong style={{color: '#E0457B'}}>Unknown public key</strong>
   );
 }
 
